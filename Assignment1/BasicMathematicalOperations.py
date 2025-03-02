267d6b05e2397e005c7495568cff6376538b44b3
Kamli <RinkeshkumarR.Kamli@harman.com>
2025-03-02 14:07:36 +0530

    Assigment 1

---
+++ Assignment1/BasicMathematicalOperations.py
@@ -0,0 +1,25 @@
+'''
+Task 1 :Basic Mathematical Operations
+    Take 2 number as input from user and perform basic mathematical operations
+    such as Addition,Subtraction,Multiplication,Division
+'''
+
+Num1 = float(input("Enter First Number:"))
+Num2 = float(input("Enter Second Number:"))
+
+print("\n")
+# Addition
+add = Num1 + Num2
+print("Addition:", add)
+
+# Subtraction
+subtract = Num1 - Num2
+print("Subtraction:", subtract)
+
+# Multiplication
+multiply = Num1 * Num2
+print("Multiplication:", multiply)
+
+# Division
+divide = Num1 / Num2
+print("Division:", divide)
---
+++ Assignment1/PersonalizedGreeting.py
@@ -0,0 +1,10 @@
+'''
+Task 2 :Personalized Greeting for the User
+'''
+
+firstName = str(input("Enter your First Name:"))
+lastName = str(input("Enter your Last Name:"))
+
+fullName = firstName +" " + lastName
+
+print("Hello ,{}! Welcome to the Python Program".format(fullName))
